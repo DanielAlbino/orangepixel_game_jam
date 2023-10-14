2ab3d1050601e20582271df8f721152b3a593ea7
Daniel Albino <daniel18albino@gmail.com>
2023-10-14 16:02:22 +0100

    added coind animation, created bullet prefab.

--- Assets/Scenes/Enemy.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-const SPEED = 100
-
-func _physics_process(delta):
-	move_and_slide()
---
+++ Assets/Scripts/Enemy.gd
@@ -0,0 +1,75 @@
+extends CharacterBody2D
+@onready var spr = $AnimatedSprite2D
+const speed = 100
+var isAttacking = false
+var timer
+var choosenMove
+var life = 50
+
+
+func _ready():
+	timer = 5
+	choosenMove = randi_range(0,4)
+
+func _physics_process(delta):
+	if !isAttacking:
+		if timer <= 0:
+			timer = randi_range(3,5)
+			choosenMove = randi_range(0,4)
+		else:
+			timer -= 0.05
+			handleBasicMovement(choosenMove)
+	var collisions = move_and_collide(velocity*delta)
+	if collisions != null:
+		timer = randi_range(3,5)
+		choosenMove = randi_range(0,4)
+
+	
+	# ['idle','move_up', 'move_down', 'move_left', 'move_right']
+func handleBasicMovement(move):
+	# Idle Enemy
+	if move == 0:
+		spr.play('Idle')
+		velocity.x = 0
+		velocity.y = 0
+		
+	# Enemy move up
+	if move == 1:
+		spr.play('Run')
+		velocity.x = 0
+		velocity.y = -speed
+	
+	# Enemy move down
+	if move == 2:
+		spr.play('Run')
+		velocity.x = 0
+		velocity.y = speed
+		
+	# Enemy move Left
+	if move == 3:
+		spr.play('Run')
+		spr.flip_h = true
+		velocity.x = -speed
+		velocity.y = 0
+	
+	# Enemy move down
+	if move == 4:
+		spr.play('Run')
+		spr.flip_h = false
+		velocity.x = speed
+		velocity.y = 0
+	
+		
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Martin":
+		attackPlayer()# Replace with function body.
+
+
+func attackPlayer():
+	var player = get_tree().get_root().get_node("res://Martin.tscn")
+	print( self.global_position)
+	#print(player.global_position)
+	#print((player.global_position - self.global_position).normalized())
+	#var dir = (player.global_position - self.global_position).normalized()
+	# move_and_collide(dir * speed)
